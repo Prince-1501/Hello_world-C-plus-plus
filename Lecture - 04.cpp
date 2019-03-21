17382f6f5174837c26e63ca58e23be67104fb5bd
Prince-1501 <37762770+Prince-1501@users.noreply.github.com>
2019-03-21 10:36:17 +0530

    Create Lecture - 04.cpp

---
+++ Lecture - 04.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+// iostream -> input output stream
+using namespace std;
+// header file --> conatins information
+
+
+// input cin-> object     scanf()
+// output cout-> object   printf()--> function
+
+// * -- coments -- *
+
+/*bd
+ njvjdn
+ bchdbc
+ bdcd */
+// std--> standard input output
+// main()  -->  execution of the program
+//   :: --> scope resolution operator
+
+// cout <<  insertion operator
+// cin  >>    extraction operator
+// ( ) --> symbolies it is function.
+int main()
+{
+    //int a;
+    std::cout<<"HELLO WORLD"<<endl;
+    //cin>>a;
+    return 0;
+}
+
