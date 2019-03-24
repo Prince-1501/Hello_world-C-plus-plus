42e715ab7402c7c2935846cd479cfd553953eb02
Prince-1501 <37762770+Prince-1501@users.noreply.github.com>
2019-03-24 09:48:48 +0530

    Create Lecture - 09.cpp

---
+++ Lecture - 09.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  lecture -9
+//
+//  Created by Prince  Kumar on 24/03/19.
+//  Copyright © 2019 Prince  Kumar. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+typedef int piku;
+int main()
+{
+    //cout<<"HEllo world"<<endl<<endl; // line changes.
+    /*int a;
+    a = 4;
+    cout<<a<<endl;
+    piku num;
+    num=45;
+    cout<<num<<endl;
+    int prince;
+    prince=7865;
+    cout<<prince<<endl;
+    
+    int size = sizeof(int);  // size of int
+    cout<<"Size of integer data type is : "<<size<<"Byte"<<endl;
+    
+    
+    cout<<"  bgvgfcg"<<sizeof(short int)<<" Bytes"<<endl;
+    
+    */
+    
+    
+    int num , xcode;
+    //cout<<"Enter the number : "<<endl;
+    cin>>num;
+    cin>>xcode;
+    cout<<"value of num "<<num<<"  "<<xcode<<endl;
+      
+}
