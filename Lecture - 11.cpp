a7b3008c0274d65c17de45b4b7ff9099b6eeb839
Prince-1501 <37762770+Prince-1501@users.noreply.github.com>
2019-04-06 09:58:27 +0530

    Create Lecture - 11.cpp

---
+++ Lecture - 11.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  lecture 11
+//
+//  Created by Prince  Kumar on 05/04/19.
+//  Copyright © 2019 Prince  Kumar. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // loop --> initalisation , condition , incrememnt
+    /*int i;
+    for (i=0; i<=10 ; i++)
+    {
+        cout<<i<<endl;
+    }*/
+    
+    
+    /*int i;
+     for (i=10; i>=0 ; i--)
+     {
+     cout<<i<<endl;
+     }*/
+    
+    /*for ( int i=0 ;i<=10 ;i++)
+    {
+        cout<<i<<endl;
+        
+    }*/
+    
+    for ( int i=0;i<=10 ;i=i+2)
+        
+    {
+        cout<<i<<endl;
+    }
+
+}
