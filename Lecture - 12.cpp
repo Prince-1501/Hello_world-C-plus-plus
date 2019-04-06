45eced9bedd5e663bb2c861801004a21e7afc111
Prince-1501 <37762770+Prince-1501@users.noreply.github.com>
2019-04-06 12:24:33 +0530

    Create Lecture - 12.cpp

---
+++ Lecture - 12.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  lecture 11
+//
+//  Created by Prince  Kumar on 05/04/19.
+//  Copyright © 2019 Prince  Kumar. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // loop --> initalisation , condition , incrememnt
+   /* int i=0;   // initalisation
+    while(i<=10)    // condition
+    {
+        cout<<i<<endl;   // statemnet
+        i= i+1;  // i++  //incrememnt
+    }
+    */
+    while(1)   // 0--> false  // 1->> true
+    {
+        cout<<"prince"<<endl; // statemnet
+        break;
+    }
+    cout<<"agarwal"<<endl;
+    
+    while(124)   // 0--> false  // 1->> true
+    {
+        cout<<"prince"<<endl; // statemnet
+        break;
+    }
+    cout<<"agarwal"<<endl;
+    int n=10;
+    while(n!=0) // 10 !=0  ->> true
+    {
+        cout<<n<<endl;
+        n--;
+    }
+
+}
