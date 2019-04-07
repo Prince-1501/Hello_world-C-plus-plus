8a3c03d1f88775ddccac673f1fe36859c051e595
Prince-1501 <37762770+Prince-1501@users.noreply.github.com>
2019-04-07 15:53:15 +0530

    Create Lecture - 16.cpp

---
+++ Lecture - 16.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  lecture 16
+//
+//  Created by Prince  Kumar on 07/04/19.
+//  Copyright © 2019 Prince  Kumar. All rights reserved.
+//
+// -- ** we discuss on strings ** --- //
+#include <iostream>
+using namespace std;
+int main()
+{
+    // we are going to create string variable
+   /*
+    string str;
+    str="prince";
+    cout<<str[3]<<endl; //  n
+    str[3]='m';
+    cout<<str[3]<<endl;  // m
+    
+    cout<<"new string is : "<<str<<endl;
+    
+    
+    */
+    
+    string s;
+    cout<<"Enter the string : "<<endl;
+    cin>>s;
+    cout<<"Our string value is : "<<s<<endl;
+}
